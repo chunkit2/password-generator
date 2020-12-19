1419e1833600dc98b67623dda409d7a42e786b39
Chunkit <chunkit.sharda@yahoo.com>
2020-12-18 20:11:05 -0800

    added variables for special, lowercase, uppercase, number, and length characters

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var specialChar = [!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var numberChar = ["123456789"]
+var charLength = ""
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
