8b393fcf519cdb57da5eded914942268ee26c91f
Chunkit <chunkit.sharda@yahoo.com>
2020-12-18 22:43:29 -0800

    log all user confirm parameters in variables.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialChar = [!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
 var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var numberChar = ["123456789"]
@@ -22,9 +22,35 @@ function generatePassword(){
 alert("Password must be between 8-128 characters");
 // re enter correct character if character requirement not met.
 var lengthAsk = window.prompt("Between 8-128 characters, how many characters should this password contain? ");  
-
-  }
+ }
+ // parameters of the password
+ var includeSpecialChar = window.confirm("Do you want to include special characters in the password?");
+ var includeLowerCase = window.confirm("Do you want to include lower case characters in the password?");
+ var includeUpperCase = window.confirm("Do you want to include upper case characters in the password?");
+ var includeNumbers = window.confirm("Do you want to include numbers in the password?");
+ // If everything is false the while loop will run causing the confirms to re ask.
+ while(includeSpecialChar===false&&includeLowerCase===false&&includeUpperCase===false&&includeNumbers===false){
+   alert("Choose at least 1 parameter");
+ var includeSpecialChar = window.confirm("Do you want to include special characters in the password?");
+ var includeLowerCase = window.confirm("Do you want to include lower case characters in the password?");
+ var includeUpperCase = window.confirm("Do you want to include upper case characters in the password?");
+ var includeNumbers = window.confirm("Do you want to include numbers in the password?");
+ }
+var passwordCombination = []
+if(includeSpecialChar){
+  passwordCombination = passwordCombination + specialChar
 }
-
+if(includeLowerCase){
+  passwordCombination = passwordCombination + lowerCase
+}
+if(includeUpperCase){
+  passwordCombination = passwordCombination + upperCase
+}
+if(includeNumbers){
+  passwordCombination = passwordCombination + numberChar
+}
+console.log (passwordCombination);
+var randPass = "";
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
