91c47f0e28cb4c7a763ac3e184d17a34fdddc2a6
Chunkit <chunkit.sharda@yahoo.com>
2020-12-18 23:30:56 -0800

    added a read.me

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-generator
+## description
+In this project I made a password generator using variables, loops, and if statements. My goal was to make a application that presented a series of prompts for password criteria. Then the aplication would take those user inputs and generate a random password. I had some dificulties with the correct notations (when to use parathesis vs brackets vs curley brackets) but the practice made me more aware of when to use each specific one. I also strugled with only getting an undefined output. If I were to change something I would make the code more easily readable by choosing less similar variable names.  
--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var specialChar = ['!#$%&']
 var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var numberChar = ["123456789"]
 var charLength = "";
-
-// Write password to the #password input  
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+ 
 // created function to prompt char amount and store into var lengthAsk
 function generatePassword(){
-  var lengthAsk = window.prompt("Between 8-128 characters, how many characters should this password contain? ");  
+  var lengthAsk = window.prompt("Between 8-128 characters, how many characters should this password contain?");  
 // while-everything is true besides whats in parathesis. || = "or"
   while (lengthAsk <8 || lengthAsk>128){ 
 alert("Password must be between 8-128 characters");
@@ -51,6 +43,17 @@ if(includeNumbers){
 }
 console.log (passwordCombination);
 var randPass = "";
+for(i=0; i<lengthAsk; i++){
+  randPass = randPass + passwordCombination[Math.floor(Math.random * passwordCombination.length)];
+  console.log(randPass);
+}
+return randPass;
+}
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
