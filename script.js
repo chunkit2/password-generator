f10c747d4dfda0fb4c1e0012859ea22e50ffd475
Chunkit <chunkit.sharda@yahoo.com>
2020-12-18 22:05:11 -0800

    added functions for character length

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var specialChar = [!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
 var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var numberChar = ["123456789"]
-var charLength = ""
+var charLength = "";
 
-// Write password to the #password input
+// Write password to the #password input  
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+// created function to prompt char amount and store into var lengthAsk
+function generatePassword(){
+  var lengthAsk = window.prompt("Between 8-128 characters, how many characters should this password contain? ");  
+// while-everything is true besides whats in parathesis. || = "or"
+  while (lengthAsk <8 || lengthAsk>128){ 
+alert("Password must be between 8-128 characters");
+// re enter correct character if character requirement not met.
+var lengthAsk = window.prompt("Between 8-128 characters, how many characters should this password contain? ");  
+
+  }
 }
 
 // Add event listener to generate button
